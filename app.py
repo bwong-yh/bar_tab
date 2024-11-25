859f3dae6e155c1091d0f5e8d0ec112d90144273
Billy Wong <bwong.yh@gmail.com>
2024-11-25 08:16:40 -0700

    convert app into oop

--- app.py
+++ app.py
@@ -1,71 +1,71 @@
-from pathlib import Path
 import csv
+from pathlib import Path
+import pandas as pd
 
-def create_csv(path, drinks, sum, tip, total):
-    with path.open('a+', newline='') as file:
-        writer = csv.writer(file)
+class BarTab():
+    def __init__(self, table_number):
+        self.table_number = table_number
+        self.drinks = []
+        self.sum = 0
+        self.tip = 0
+        self.total = 0
 
-        writer.writerow(['Drink', 'Price'])
-        writer.writerows(drinks)
-        writer.writerow(['Sum', sum])
-        writer.writerow(['Tip', tip])
-        writer.writerow(['Total', total])
-        print('CSV created')
+    def get_receipt(self):
+        path = Path(__file__).parent / f'table_{self.table_number}.csv'
 
+        data = pd.read_csv(path)
+        print(data.to_string(index=False))
 
-def calculate_total(drinks, tip=0.2):
-    sum = 0
+    def create_csv(self):
+        if not self.drinks:
+            print('Nothing added.')
+            return
 
-    for drink, price in drinks:
-        sum += price
+        path = Path(__file__).parent / f'table_{self.table_number}.csv'
 
-    return sum, tip, sum * (1 + tip)
+        with path.open('w+', newline='') as file:
+            writer = csv.writer(file)
 
-def serve_user():
-    drinks = []
-    
-    while True:
-        drink = input('Drink (press f to finish): ').strip().lower()
+            writer.writerow(['Drink', 'Price'])
+            writer.writerows(self.drinks)
+            writer.writerow(['Sum', self.sum])
+            writer.writerow(['Tip', self.tip])
+            writer.writerow(['Total', self.total])
+            print('CSV created.')
 
-        if drink == 'f':
-            break
+    def calculate_total(self, tip=0.2):
+        for _, price in self.drinks:
+            self.sum += price
 
-        try:
-            price = float(input(f'{drink.title()} price: ').strip())
-            drinks.append((drink.title(), price))        
-        except ValueError:
-            print('Invalid price')
-            continue
+        self.tip = self.sum * tip
+        self.total = self.sum + self.tip
 
-    return drinks
+    def server_user(self):
+        while True:
+            drink = input('Drink (press f to finish): ').strip().lower()
+
+            if drink == 'f':
+                break
+
+            try:
+                price = float(input(f'{drink.title()} price: ').strip())
+                self.drinks.append((drink.title(), price))        
+            except ValueError:
+                print('Invalid price.')
+                continue
 
 def main():
-
-
-    # get table number
     try:
-        table_number = int(input('Table no: '))
-        print(f'Starting tab for table {table_number}.')
+        table_number = int(input('Enter table number: ').strip())
+        tab = BarTab(table_number)
+        print(f'New tab created for table {tab.table_number}.')
+
+        tab.server_user()
+        tab.calculate_total()
+        tab.create_csv()
+        tab.get_receipt()
     except ValueError:
-        print('Not a valid table number. Exiting program.')
-
-    # create path
-    path = Path(__file__).parent / f'table_{table_number}.csv'
-
-    # ask for drinks and prices
-    drinks = serve_user()
-
-    # calculate tip and total  
-    if not drinks:
-        print('No drinks added. Exiting program.')
-        return
-
-    sum, tip, total = calculate_total(drinks)
-
-    # create csv
-    create_csv(path, drinks, sum, tip, total)
-
-    return
+        print('Not a valid table number.')
 
 if __name__ == '__main__':
     main()
